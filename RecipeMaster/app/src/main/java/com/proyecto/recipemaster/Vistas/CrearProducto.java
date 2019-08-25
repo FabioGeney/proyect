e1859aec316f1360ab65bed42c3203cf395eba90
FabioGeney <37422482+FabioGeney@users.noreply.github.com>
2019-08-24 22:10:51 -0500

    awcons

---
+++ RecipeMaster/app/src/main/java/com/proyecto/recipemaster/Vistas/CrearProducto.java
@@ -0,0 +1,16 @@
+package com.proyecto.recipemaster.Vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyecto.recipemaster.R;
+
+public class CrearProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_producto);
+    }
+}
